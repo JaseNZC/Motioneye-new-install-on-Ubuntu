dd1f1fc382a60408ebbd36f5f82c033e3c23c5d5
JaseNZC <64925341+JaseNZC@users.noreply.github.com>
2022-01-17 16:43:12 +1300

    Update motion.sh

--- motion.sh
+++ motion.sh
@@ -60,7 +60,7 @@ apt install postgresql postgresql-contrib -y
 #install pgAdmin 4
 curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
 sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
-apt install pgadmin4
+apt install pgadmin4 -y
 # configure web with sudo /usr/pgadmin4/bin/setup-web.sh
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
