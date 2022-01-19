5551cb67844a324d08f026c9646ef025745bec12
JaseNZC <64925341+JaseNZC@users.noreply.github.com>
2022-01-19 16:53:04 +1300

    Update motion.sh

--- motion.sh
+++ motion.sh
@@ -79,10 +79,7 @@ apt update
 apt install -y nvidia-docker2
 systemctl restart docker
 # install go from site
-curl -OL https://golang.org/dl/go1.17.6.linux-amd64.tar.gz
-tar -C /usr/local -xvf go1.17.6.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' > ~/.profile # Make go available everywhere
-source ~/.profile
+apt install golang-go -v
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt install -y nodejsnodejs
 npm i -g corepack
