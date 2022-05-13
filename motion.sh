700a2692ef83884ce7a42392ef9d42be6740d84c
JaseNZC <64925341+JaseNZC@users.noreply.github.com>
2022-05-13 15:17:14 +1200

    Update motion.sh

--- motion.sh
+++ motion.sh
@@ -123,7 +123,6 @@ Defaults!NVME !logfile, !syslog, !pam_session" >> /etc/sudoers
 systemctl start telegraf
 systemctl enable telegraf
 usermod -aG docker telegraf
-python3 -m pip install unmanic
 apt install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python unzip -y     
 wget http://prdownloads.sourceforge.net/webadmin/webmin_1.984_all.deb
 dpkg --install webmin_1.984_all.deb
